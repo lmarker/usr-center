68ca94681d23c73405fd17d9c01cfe4be883d5f2
lijiahao <lijiahao_0606@qq.com>
2020-04-19 01:55:42 +0800

    接入服务层

---
+++ usr-center-core/src/main/java/com/maksdu/usr/center/core/proxy/dto/WeChatJsCode2SessionDTO.java
@@ -0,0 +1,16 @@
+package com.maksdu.usr.center.core.proxy.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class WeChatJsCode2SessionDTO implements Serializable {
+
+    private String openid;
+
+    private String session_key;
+}
